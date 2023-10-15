852c301c46b1365ad6215baad98899c87395e863
Achyuta <achyutathegreat@gmail.com>
2023-10-15 12:30:59 +0530

    feat: individualise services

--- server/main.go
+++ server/main.go
@@ -1,14 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
+
+var name string
 
 func RequestHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Hello World"))
+    w.Write([]byte("Hello World from Server " + fmt.Sprint(name)))
 }
 
 func main() {
+    name = os.Getenv("NAME")
     r := http.NewServeMux()
     r.Handle("/", http.HandlerFunc(RequestHandler))
     http.ListenAndServe(":8000", r)
